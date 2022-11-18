d1ad97ec1e5b561d16f727d1b6a42a5c9c8f85d8
John Patrick Madrigal <jp.madrigal07@gmail.com>
2022-11-18 19:36:24 +0800

    fix: tailwind intellisense

---
+++ apps/land/tailwind.config.js
@@ -0,0 +1,4 @@
+// This file is not the main config for this app
+// This was put just to be seen by tailwind intellisense
+// This app uses the config on packages/tailwind-config
+module.exports = {};
