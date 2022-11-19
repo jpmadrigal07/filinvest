d26823903b2ac4dba0e9ccb8098dd6af987c0a3d
John Patrick Madrigal <jp.madrigal07@gmail.com>
2022-11-19 19:26:46 +0800

    feat: Added financials annual reports page

---
+++ apps/land/helpers/routes/financials.ts
@@ -0,0 +1,22 @@
+export const FINANCIALS = {
+  url: "/financials",
+  name: "Financials",
+  directory: ["Financials"],
+};
+const FINANCIAL_HIGHLIGHTS = {
+  url: `${FINANCIALS.url}/financial-highlights`,
+  name: "Financial Highlights",
+  directory: [...FINANCIALS.directory, "Financial Highlights"],
+};
+const ANNUAL_REPORTS = {
+  url: `${FINANCIALS.url}/annual-reports`,
+  name: "Annual Reports",
+  directory: [...FINANCIALS.directory, "Annual Reports"],
+};
+const BUYERS_ROUTES = {
+  ...FINANCIALS,
+  FINANCIAL_HIGHLIGHTS,
+  ANNUAL_REPORTS,
+};
+export const FINANCIALS_SUB_ROUTES = [FINANCIAL_HIGHLIGHTS, ANNUAL_REPORTS];
+export default BUYERS_ROUTES;
