3341d61bd732099ffe4f519382fe81f6ab00acd5
John Patrick Madrigal <jp.madrigal07@gmail.com>
2022-11-25 20:51:16 +0800

    feat: Added office park single page

---
+++ apps/land/app/our-businesses/offices/office-parks/north-gate/page.tsx
@@ -0,0 +1,13 @@
+import MainHeader from "@/components/header/MainHeader";
+import Content from "@/components/pages/our-businesses/offices/office-parks/north-gate/Content";
+
+const OfficeParksPage = () => {
+  return (
+    <>
+      <MainHeader isBlueHeader />
+      <Content />
+    </>
+  );
+};
+
+export default OfficeParksPage;
