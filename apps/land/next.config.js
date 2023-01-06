f02d126304d7cdc4c03804caeb2291c89135dc6b
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-01-06 12:17:11 +0800

    fix: Css bug for payloadcms

--- apps/land/next.config.js
+++ apps/land/next.config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: "../../.env",
+});
+
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
@@ -11,12 +15,12 @@ module.exports = {
     return [
       {
         source: `/api/:path*`,
-        destination: `http://localhost:9000/api/:path*`,
+        destination: `${process.env.CMS_API_URL}/api/:path*`,
         basePath: false,
       },
       {
         source: `/files/:path*`,
-        destination: `http://localhost:9000/files/:path*`,
+        destination: `${process.env.CMS_API_URL}/files/:path*`,
         basePath: false,
       },
     ];
