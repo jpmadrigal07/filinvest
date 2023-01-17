9affc73058e03deef73012a869d3e3116c900d7b
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-01-17 11:11:47 +0800

    fix: Property search web view

--- apps/land/components/pages/property-search/Content.tsx
+++ apps/land/components/pages/property-search/Content.tsx
@@ -1,83 +1,18 @@
 "use client";
-import RangeSliderMark from "@/components/range-sliders/RangeSliderMark";
-import ChevronDown from "@/components/svg/ChevronDown";
+import PropertySearch from "@/components/search/PropertySearch";
 import Flag from "@/components/svg/Flag";
 import Peso from "@/components/svg/Peso";
 import SizeBox from "@/components/svg/SizeBox";
-import { toCurrency } from "@/helpers/homeCalculator";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const Content = () => {
-  const [priceRange, setPriceRange] = useState([20, 50]);
-  const [priceCurrencyRange, setPriceCurrencyRange] = useState([0, 0]);
-  useEffect(() => {
-    const pricePerValue = 1000000;
-    const min = priceRange[0] * pricePerValue;
-    const max = priceRange[1] * pricePerValue;
-    setPriceCurrencyRange([min, max]);
-  }, [priceRange]);
-
   return (
-    <section className="-mt-24 flex flex-col gap-9 2xl:-mt-32">
-      <div className="bg-dark-cornflower-blue mx-16 flex items-center gap-8 px-10 py-6 lg:mx-9 xl:mx-16 2xl:mx-44">
-        <div className="w-full flex-1">
-          <h3 className="text-white">Property Type</h3>
-          <div className="border-silver-chalice flex items-center gap-3 border-b-[1px] py-2 hover:cursor-pointer">
-            <input
-              type="text"
-              placeholder="Condominium"
-              className="ring-none w-full border-none bg-transparent text-white hover:cursor-pointer"
-            />
-            <ChevronDown />
-          </div>
-        </div>
-        <div className="w-full flex-1">
-          <h3 className="text-white">Location</h3>
-          <div className="border-silver-chalice flex items-center gap-3 border-b-[1px] py-2 hover:cursor-pointer">
-            <input
-              type="text"
-              placeholder="Cebu"
-              className="ring-none w-full border-none bg-transparent text-white hover:cursor-pointer"
-            />
-            <ChevronDown />
-          </div>
-        </div>
-        <div className="w-full flex-1">
-          <h3 className="text-white">Unit Size</h3>
-          <div className="border-silver-chalice flex items-center gap-3 border-b-[1px] py-2 hover:cursor-pointer">
-            <input
-              type="text"
-              placeholder="Any"
-              className="ring-none w-full border-none bg-transparent text-white hover:cursor-pointer"
-            />
-            <ChevronDown />
-          </div>
-        </div>
-        <div className="w-full flex-1">
-          <h3 className="mb-1 text-white">Price Range</h3>
-          <RangeSliderMark
-            range
-            defaultValue={priceRange}
-            onValueChange={setPriceRange}
-            value={priceRange}
-          />
-          <h4 className="mt-1 text-sm text-white">
-            Php {toCurrency(priceCurrencyRange[0])} - Php{" "}
-            {toCurrency(priceCurrencyRange[1])}
-          </h4>
-        </div>
-        {/* <div className="flex-none">
-          <button className="hover:bg-platinum focus:bg-platinum delay-50 bg-white py-4 px-8 transition">
-            <div className="text-dark-cornflower-blue flex items-center gap-2 font-bold">
-              <Search /> Search
-            </div>
-          </button>
-        </div> */}
-      </div>
-      <div className="mt-16 lg:mx-9 xl:mx-16 2xl:mx-44">
+    <section className="-mt-24 flex flex-col gap-9 2xl:-mt-44">
+      <PropertySearch showSearch={false} className="mx-9 lg:mx-0" />
+      <div className="mx-9 mt-16 xl:mx-16 2xl:mx-44">
         <p>4 properties found</p>
-        <div className="mt-7 grid grid-cols-3 gap-8">
+        <div className="mt-7 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
           <div className="bg-white shadow-xl">
             <div>
               <Image
@@ -217,7 +152,7 @@ const Content = () => {
         </div>
       </div>
       <div className="bg-ghost-white mt-24 flex flex-col items-center py-24">
-        <div className="w-1/3">
+        <div className="mx-6 md:mx-9 lg:mx-0 lg:w-1/3">
           <h2 className="text-jet text-center text-4xl font-bold">
             Recommendation
           </h2>
@@ -235,7 +170,7 @@ const Content = () => {
             cupidatat non proident.
           </p>
         </div>
-        <div className="mt-16 grid grid-cols-3 gap-8 lg:mx-9 xl:mx-16 2xl:mx-44">
+        <div className="mx-9 mt-16 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:mx-16 2xl:mx-44">
           <div className="bg-white shadow-xl">
             <div>
               <Image
