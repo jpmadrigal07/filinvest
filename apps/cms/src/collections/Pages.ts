cdfdcd03fb00e05474a7b9f9d7976f7626e7919b
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-02-11 09:19:24 +0800

    feat: setup office parks cms

--- apps/cms/src/collections/Pages.ts
+++ apps/cms/src/collections/Pages.ts
@@ -15,6 +15,7 @@ import {
   HomeStockReportSection,
   HomeFloatingButtons,
 } from "../blocks/Home";
+import { OfficeParksOfficeParks } from "../blocks/OfficeParks";
 import Header from "../blocks/Header";
 
 export const Pages: CollectionConfig = {
@@ -59,6 +60,7 @@ export const Pages: CollectionConfig = {
         HomeInvestorRelationSection,
         HomeStockReportSection,
         HomeFloatingButtons,
+        OfficeParksOfficeParks,
       ],
     },
     {
