7b2f0f89761153c44fbafc9c3e1852f720ea281f
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-02-14 08:08:32 +0800

    fix: fixed slug and removed locations

--- apps/cms/src/blocks/Townscapes/TownscapesTownscapes.ts
+++ apps/cms/src/blocks/Townscapes/TownscapesTownscapes.ts
@@ -5,19 +5,6 @@ const TownscapesTownscapes: Block = {
   fields: [
     {
       type: "array",
-      name: "locations",
-      label: "Locations",
-      required: true,
-      fields: [
-        {
-          name: "Location",
-          type: "text",
-          required: true,
-        },
-      ],
-    },
-    {
-      type: "array",
       name: "townscapes",
       label: "Townscapes",
       required: true,
