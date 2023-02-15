ba054770c5d5307fc9840a4f68cf8a97fcf54919
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-02-15 09:36:34 +0800

    feat: setup disclosures cms

--- apps/cms/src/collections/Pages.ts
+++ apps/cms/src/collections/Pages.ts
@@ -15,6 +15,7 @@ import {
   HomeStockReportSection,
   HomeFloatingButtons,
 } from "../blocks/Home";
+import { DisclosuresTable } from "../blocks/Disclosures";
 import { OurLeadershipBoardOfDirectors } from "../blocks/OurLeadership";
 import Header from "../blocks/Header";
 
@@ -61,6 +62,7 @@ export const Pages: CollectionConfig = {
         HomeStockReportSection,
         HomeFloatingButtons,
         OurLeadershipBoardOfDirectors,
+        DisclosuresTable,
       ],
     },
     {
