b41def2a7890ee3eebb2f9ca01ff16584c92f2dc
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-02-19 07:12:00 +0800

    refactor: removed console log

--- apps/land/components/pages/about-us/dreams-built-green/MediaSection.tsx
+++ apps/land/components/pages/about-us/dreams-built-green/MediaSection.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Image from "next/image";
 
 const MediaSection = ({ content }: any) => {
-  console.log(content.content[2].backgroundMedia.url);
   return (
     <div
       className={`h-[1080px] w-full bg-[image:${content.content[2].backgroundMedia.url}] bg-opacity-100 bg-cover bg-no-repeat`}
