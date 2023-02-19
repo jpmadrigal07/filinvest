374b5ad880214f917309c1b317d4ceb5426ae7a1
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-02-19 15:23:22 +0800

    refactor: connect malls cms to ui

--- apps/land/app/our-businesses/malls/page.tsx
+++ apps/land/app/our-businesses/malls/page.tsx
@@ -1,13 +1,23 @@
 import MainHeader from "@/components/header/MainHeader";
 import { HEADER_INFO } from "@/components/pages/our-businesses/constants";
 import Content from "@/components/pages/our-businesses/malls/Content";
+import TitleText from "@/components/pages/our-businesses/malls/TitleText";
 
-const MallsPage = () => {
+async function getPageContent(id: string) {
+  const res = await fetch(`${process.env.CMS_API_URL}/api/pages/${id}`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+const MallsPage = async () => {
+  const content = await getPageContent("63f1ce3cfa79c21ee7bb5dd0");
   const { title, breadcrumbs, image } = HEADER_INFO.malls;
   return (
     <>
       <MainHeader title={title} breadcrumbs={breadcrumbs} bgUrl={image} />
-      <Content />
+      <TitleText content={content} />
     </>
   );
 };
