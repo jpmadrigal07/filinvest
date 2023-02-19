44bb629205793bc7877572274fce8b2c47d1f1a4
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-02-19 16:32:29 +0800

    refactor: connect townscapes cms to ui

--- apps/land/app/our-businesses/townscapes/page.tsx
+++ apps/land/app/our-businesses/townscapes/page.tsx
@@ -1,13 +1,23 @@
 import MainHeader from "@/components/header/MainHeader";
 import { HEADER_INFO } from "@/components/pages/our-businesses/constants";
 import Content from "@/components/pages/our-businesses/townscapes/Content";
+import TitleText from "@/components/pages/our-businesses/townscapes/TitleText";
 
-const TownscapesPage = () => {
+async function getPageContent(id: string) {
+  const res = await fetch(`${process.env.CMS_API_URL}/api/pages/${id}`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+const TownscapesPage = async () => {
+  const content = await getPageContent("63f1dc7cfa79c21ee7bb5ec1");
   const { title, breadcrumbs, image } = HEADER_INFO.townscapes;
   return (
     <>
       <MainHeader title={title} breadcrumbs={breadcrumbs} bgUrl={image} />
-      <Content />
+      <TitleText content={content} />
     </>
   );
 };
