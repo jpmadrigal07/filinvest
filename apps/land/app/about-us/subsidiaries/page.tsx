9eb9a9a28626919d85efc740f031478d9ecfdd91
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-02-19 13:15:36 +0800

    refactor: connect subsidiaries cms to ui

--- apps/land/app/about-us/subsidiaries/page.tsx
+++ apps/land/app/about-us/subsidiaries/page.tsx
@@ -1,8 +1,17 @@
 import MainHeader from "@/components/header/MainHeader";
 import Content from "@/components/pages/about-us/subsidiaries/Content";
 import { HEADER_INFO } from "@/components/pages/about-us/constants";
+import SubsidiariesSection from "@/components/pages/about-us/subsidiaries/SubsidiariesSection";
 
-const Subsidiaries = () => {
+async function getPageContent(id: string) {
+  const res = await fetch(`${process.env.CMS_API_URL}/api/pages/${id}`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+const Subsidiaries = async () => {
+  const content = await getPageContent("639a5888b60dc36e6fc86e12");
   const { title, breadcrumbs, image, imageSmall } = HEADER_INFO.subsidiaries;
   return (
     <>
@@ -12,7 +21,8 @@ const Subsidiaries = () => {
         bgUrl={image}
         bgUrlSmall={imageSmall}
       />
-      <Content />
+      {/* <Content /> */}
+      <SubsidiariesSection content={content} />
     </>
   );
 };
---
+++ apps/land/components/pages/about-us/subsidiaries/SubsidiariesSection.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import React from "react";
+import BorderButton from "../../../button/BorderButton";
+
+const SubsidiariesSection = ({ content }: any) => {
+  return (
+    <section className="mt-16 mb-28 flex flex-col gap-6">
+      <div className="mx-6 grid gap-6 md:grid-cols-2 lg:mx-9 lg:grid-cols-3 xl:mx-16 2xl:mx-44">
+        {content.content[0].subsidiariesCompanies.map((item: any) => (
+          <div className="bg-ghost-white flex flex-col gap-10 px-7 pt-6 pb-12">
+            <Image
+              src={`${item.companyLogo.url}`}
+              width={74}
+              height={74}
+              alt={item.companyLogo.alt}
+            />
+            <div>
+              <h3 className="text-jet text-4xl font-bold">
+                {item.companyName}
+              </h3>
+              <p className="text-dim-gray mt-2">{item.companyDescription}</p>
+            </div>
+            <div>
+              <button type="button">
+                <BorderButton
+                  buttonText="Learn More"
+                  textColor="dark-cornflower-blue"
+                  borderColor="dark-cornflower-blue"
+                />
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default SubsidiariesSection;
