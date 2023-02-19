f983b5ac77599dae90a066b360c04b7025117047
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-02-19 21:01:55 +0800

    refactor: connect annual corporate governance report cms to ui

--- apps/land/app/investor-relations/corporate-governance/annual-corporate-governance-report/page.tsx
+++ apps/land/app/investor-relations/corporate-governance/annual-corporate-governance-report/page.tsx
@@ -1,8 +1,18 @@
 import MainHeader from "@/components/header/MainHeader";
 import { HEADER_INFO } from "@/components/pages/investor-relations/corporate-governance/constants";
 import Content from "@/components/pages/investor-relations/corporate-governance/annual-corporate-governance-report/Content";
+import AnnualCorporateGovernance from "@/components/pages/investor-relations/corporate-governance/annual-corporate-governance-report/AnnualCorporateGovernance";
 
-const AnnualCorporateGovernanceReportPage = () => {
+async function getPageContent(id: string) {
+  const res = await fetch(`${process.env.CMS_API_URL}/api/pages/${id}`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+const AnnualCorporateGovernanceReportPage = async () => {
+  const content = await getPageContent("63f21bbf9813fc62eebc9960");
   const { title, breadcrumbs, image, tabs } =
     HEADER_INFO.annualCorporateGovernanceReport;
   return (
@@ -13,7 +23,7 @@ const AnnualCorporateGovernanceReportPage = () => {
         bgUrl={image}
         tabs={tabs}
       />
-      <Content />
+      <AnnualCorporateGovernance content={content} />
     </>
   );
 };
