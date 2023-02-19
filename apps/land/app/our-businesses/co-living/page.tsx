28fe76e7ead6fe8a2a86fe71df62d0c0f1631a30
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-02-19 16:47:51 +0800

    refactor: connect co living cms to ui

--- apps/land/app/our-businesses/co-living/page.tsx
+++ apps/land/app/our-businesses/co-living/page.tsx
@@ -1,13 +1,22 @@
 import MainHeader from "@/components/header/MainHeader";
 import { HEADER_INFO } from "@/components/pages/our-businesses/constants";
-import Content from "@/components/pages/our-businesses/co-living/Content";
+import TitleText from "./TitleText";
 
-const CoLivingPage = () => {
+async function getPageContent(id: string) {
+  const res = await fetch(`${process.env.CMS_API_URL}/api/pages/${id}`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+const CoLivingPage = async () => {
+  const content = await getPageContent("63f1e1894c33ce3025f5a790");
   const { title, breadcrumbs, image } = HEADER_INFO.coliving;
   return (
     <>
       <MainHeader title={title} breadcrumbs={breadcrumbs} bgUrl={image} />
-      <Content />
+      <TitleText content={content} />
     </>
   );
 };
