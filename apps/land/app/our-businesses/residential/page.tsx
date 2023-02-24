8b6f24800ff1cb65b9dbee83bd601f64cae32817
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-02-24 16:45:01 +0800

    fix: fixed cms url

--- apps/land/app/our-businesses/residential/page.tsx
+++ apps/land/app/our-businesses/residential/page.tsx
@@ -1,13 +1,12 @@
 import MainHeader from "@/components/header/MainHeader";
 import { HEADER_INFO } from "@/components/pages/our-businesses/constants";
 import AspireHomes from "@/components/pages/our-businesses/residential/AspireHomes";
-import Content from "@/components/pages/our-businesses/residential/Content";
 import FuturaHomes from "@/components/pages/our-businesses/residential/FuturaHomes";
 import PrestigeHomes from "@/components/pages/our-businesses/residential/PrestigeHomes";
 import TitleText from "@/components/pages/our-businesses/residential/TitleText";
 
 async function getPageContent(id: string) {
-  const res = await fetch(`${process.env.CMS_API_URL}/api/pages/${id}`);
+  const res = await fetch(`${process.env.CMS_URL}/api/pages/${id}`);
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
@@ -24,7 +23,6 @@ const ResidentialPage = async () => {
       <PrestigeHomes content={content} />
       <AspireHomes content={content} />
       <FuturaHomes content={content} />
-      {/* <Content /> */}
     </>
   );
 };
