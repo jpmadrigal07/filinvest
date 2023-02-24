d5f36dd75d6c97acac6928cb5df8782a91de2ac1
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-02-24 11:40:09 +0800

    fix: fixed media section

--- apps/land/components/pages/about-us/dreams-built-green/MediaSection.tsx
+++ apps/land/components/pages/about-us/dreams-built-green/MediaSection.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import Image from "next/image";
 
 const MediaSection = ({ content }: any) => {
+  const urlYt = content.content[2]?.backgroundVideoLink;
+  const formattedUrlYt = urlYt?.includes("https://www.youtube.com/watch?v=")
+    ? urlYt.replace("https://www.youtube.com/watch?v=", "")
+    : "";
   return (
-    <div
-      className={`h-[1080px] w-full bg-[image:${content.content[2].backgroundMedia.url}] bg-opacity-100 bg-cover bg-no-repeat`}
-    ></div>
+    <>
+      <iframe
+        className="pointer-events-none bottom-0 right-0 block h-full w-full object-cover"
+        src={`https://www.youtube.com/embed/${formattedUrlYt}?autoplay=1&mute=1&cc_load_policy=1&controls=0&loop=1&showinfo=0&modestbranding=1`}
+        allow="autoplay; modestbranding"
+      ></iframe>
+    </>
   );
 };
 
