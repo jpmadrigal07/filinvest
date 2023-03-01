9bbc78713f88e7eed091168e2e5248c9010d47d4
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-03-01 17:35:41 +0800

    fix: implementation for connecting industrial cms to ui

--- apps/land/components/pages/our-businesses/industrial/TitleText.tsx
+++ apps/land/components/pages/our-businesses/industrial/TitleText.tsx
@@ -2,14 +2,17 @@
 import React from "react";
 
 const TitleText = ({ content }: any) => {
+  const data = content?.content.find(
+    (item: any) => item.blockType === "industrial-title-and-text"
+  );
   return (
     <section className="mx-9 mt-16 mb-32 xl:mx-16 2xl:mx-44">
       <div>
         <div>
           <h2 className="text-jet text-center text-4xl font-bold">
-            {content.content[0].title}
+            {data.title}
           </h2>
-          <p className="text-dim-gray mt-8">{content.content[0].description}</p>
+          <p className="text-dim-gray mt-8">{data.description}</p>
         </div>
       </div>
     </section>
