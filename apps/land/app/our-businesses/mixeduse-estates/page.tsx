72c4ac9b80f931b5bb3614e9837efb10096c7cfc
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-03-03 09:26:01 +0800

    fix: fix implementation for connecting cms to ui

--- apps/land/app/our-businesses/mixeduse-estates/page.tsx
+++ apps/land/app/our-businesses/mixeduse-estates/page.tsx
@@ -1,8 +1,8 @@
-import MainHeader from "@/components/header/MainHeader";
 import { HEADER_INFO } from "@/components/pages/our-businesses/constants";
 import Content from "@/components/pages/our-businesses/mixeduse-estates/Content";
 import qs from "qs";
 import { getRequest } from "@/helpers/getRequest";
+import { metaBuilder } from "@/helpers/metaBuilder";
 
 const query = {
   "projectType.title": {
@@ -17,22 +17,25 @@ const stringifiedQuery = qs.stringify(
   { addQueryPrefix: true }
 );
 
+async function getPageContent(id: string) {
+  const res = await fetch(`${process.env.CMS_URL}/api/pages/${id}`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
 export async function generateMetadata() {
-  return {
-    title: "Mixed-used Estates",
-    description: "Mixed-used Estates",
-  };
+  const content = await getPageContent("63f1e2b44c33ce3025f5a813");
+  return metaBuilder(content);
 }
 
 const MixeduseEstatesPage = async () => {
+  const content = await getPageContent("63f1e2b44c33ce3025f5a813");
   const projects = await getRequest(`/api/projects${stringifiedQuery}`);
   const locations = await getRequest(`/api/location-categories`);
-  const { title, breadcrumbs, image } = HEADER_INFO.mixedUseEstates;
   return (
-    <>
-      <MainHeader title={title} breadcrumbs={breadcrumbs} bgUrl={image} />
-      <Content projects={projects} locations={locations} />
-    </>
+    <Content content={content} projects={projects} locations={locations} />
   );
 };
 
--- apps/land/components/pages/our-businesses/mixeduse-estates/TitleText.tsx
+++ apps/land/components/pages/our-businesses/mixeduse-estates/TitleText.tsx
@@ -2,17 +2,18 @@
 import React from "react";
 
 const TitleText = ({ content }: any) => {
+  const data = content?.content.find(
+    (item: any) => item.blockType === "mixed-used-and-estates-title-and-text"
+  );
   return (
-    <section className="mx-9 mt-16 mb-32 xl:mx-16 2xl:mx-44">
+    <>
       <div>
-        <div>
-          <h2 className="text-jet text-center text-4xl font-bold">
-            {content.content[0].title}
-          </h2>
-          <p className="text-dim-gray mt-8">{content.content[0].description}</p>
-        </div>
+        <h2 className="text-jet text-center text-4xl font-bold">
+          {data.title}
+        </h2>
+        <p className="text-dim-gray mt-8">{data.description}</p>
       </div>
-    </section>
+    </>
   );
 };
 
