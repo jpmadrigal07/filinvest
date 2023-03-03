949116996c96cc6ce852b34f539a07a909347ee4
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-03-03 23:32:16 +0800

    fix: implementation for connecting cms to ui

--- apps/land/app/investor-relations/corporate-governance/annual-corporate-governance-report/page.tsx
+++ apps/land/app/investor-relations/corporate-governance/annual-corporate-governance-report/page.tsx
@@ -1,6 +1,6 @@
-import MainHeader from "@/components/header/MainHeader";
 import { HEADER_INFO } from "@/components/pages/investor-relations/corporate-governance/constants";
 import Content from "@/components/pages/investor-relations/corporate-governance/annual-corporate-governance-report/Content";
+import { metaBuilder } from "@/helpers/metaBuilder";
 
 async function getPageContent(id: string) {
   const res = await fetch(`${process.env.CMS_URL}/api/pages/${id}`);
@@ -11,25 +11,16 @@ async function getPageContent(id: string) {
 }
 
 export async function generateMetadata() {
-  return {
-    title: "Annual Corporate Governance",
-    description: "Annual Corporate Governance",
-  };
+  const content = await getPageContent("639a584bb60dc36e6fc86d90");
+  return metaBuilder(content);
 }
 
 const AnnualCorporateGovernanceReportPage = async () => {
   const content = await getPageContent("63f21bbf9813fc62eebc9960");
-  const { title, breadcrumbs, image, tabs } =
-    HEADER_INFO.annualCorporateGovernanceReport;
+  HEADER_INFO.annualCorporateGovernanceReport;
   return (
     <>
-      <MainHeader
-        title={title}
-        breadcrumbs={breadcrumbs}
-        bgUrl={image}
-        tabs={tabs}
-      />
-      <Content />
+      <Content content={content} />
     </>
   );
 };
--- apps/land/components/pages/investor-relations/corporate-governance/annual-corporate-governance-report/AnnualCorporateGovernance.tsx
+++ apps/land/components/pages/investor-relations/corporate-governance/annual-corporate-governance-report/AnnualCorporateGovernance.tsx
@@ -3,10 +3,13 @@ import Image from "next/image";
 import React from "react";
 
 const AnnualCorporateGovernance = ({ content }: any) => {
+  const data = content?.content.find(
+    (item: any) => item.blockType === "annual-corporate-governance-and-report"
+  );
   return (
     <section className="-mt-16 mb-28 flex flex-col gap-9 lg:mx-9 xl:mx-16 2xl:mx-44 2xl:-mt-52">
       <div className="grid grid-cols-4 gap-12">
-        {content.content[0].reportList.map((item) => (
+        {data.reportList.map((item: any) => (
           <div className="flex h-full flex-col items-center gap-6 bg-white p-16 px-12 shadow-xl">
             <div className="bg-dark-cornflower-blue flex w-16 flex-none items-center justify-center rounded-full px-6 py-6 shadow-2xl">
               <Image
