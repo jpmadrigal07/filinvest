79b4cbcbe891ccb9a44ffd69473877330988e7ec
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-03-03 18:12:16 +0800

    fix: implementation of connecting cms to ui

--- apps/land/app/investor-relations/corporate-governance/manual-corporate-governance/page.tsx
+++ apps/land/app/investor-relations/corporate-governance/manual-corporate-governance/page.tsx
@@ -1,5 +1,3 @@
-import MainHeader from "@/components/header/MainHeader";
-import { HEADER_INFO } from "@/components/pages/investor-relations/corporate-governance/constants";
 import Content from "@/components/pages/investor-relations/corporate-governance/manual-corporate-governance/Content";
 import { metaBuilder } from "@/helpers/metaBuilder";
 
@@ -18,16 +16,8 @@ export async function generateMetadata() {
 
 const ManualCorporateGovernancePage = async () => {
   const content = await getPageContent("63f211e17ed2c505a81c3b77");
-  const { title, breadcrumbs, image, tabs } =
-    HEADER_INFO.manualCorporateGovernance;
   return (
     <>
-      <MainHeader
-        title={title}
-        breadcrumbs={breadcrumbs}
-        bgUrl={image}
-        tabs={tabs}
-      />
       <Content content={content} />
     </>
   );
--- apps/land/components/pages/investor-relations/corporate-governance/manual-corporate-governance/Content.tsx
+++ apps/land/components/pages/investor-relations/corporate-governance/manual-corporate-governance/Content.tsx
@@ -1,96 +1,41 @@
-import BorderButton from "@/components/button/BorderButton";
-import PageNextPrevButton from "@/components/button/PageNextPrevButtonOld";
-import LinkedIn from "@/components/svg/LinkedIn";
+"use client";
+import Breadcrumbs from "@/components/header/Breadcrumbs";
+import MainHeader from "@/components/header/MainHeader";
 import React from "react";
+import { Tabs } from "react-bootstrap";
+import ManualCorporateGovernance from "./ManualCorporateGovernance";
 
-const Content = () => {
+const Content = ({ content }: any) => {
+  const header = content?.content.find(
+    (item: any) => item.blockType === "header"
+  );
+  const breadcrumbsItems = header?.breadcrumbs.map(
+    (tab: any, index: number) => {
+      return {
+        title: tab.link.label,
+        ...(index + 1 < header?.breadcrumbs?.length && { link: tab.link.url }),
+      };
+    }
+  );
+  const breadcrumbs = <Breadcrumbs items={breadcrumbsItems} />;
+  const tabItems = header?.tabs.map((tab: any) => {
+    return {
+      title: tab.link.label,
+      link: tab.link.url,
+    };
+  });
+  const tabs = <Tabs items={tabItems} />;
   return (
-    <section className="-mt-16 mb-28 flex flex-col gap-9 lg:mx-9 xl:mx-16 2xl:mx-44 2xl:-mt-52">
-      <div className="grid grid-cols-3 gap-12">
-        <div className="flex flex-col items-center gap-6 bg-white p-16 px-6 shadow-xl">
-          <div className="bg-dark-cornflower-blue flex w-16 flex-none items-center justify-center rounded-full px-6 py-6 shadow-2xl">
-            <LinkedIn />
-          </div>
-          <h3 className="text-jet text-xl font-bold">
-            Code Of Business Conduct And Ethics
-          </h3>
-          <BorderButton
-            buttonText="View"
-            textColor="dark-cornflower-blue"
-            borderColor="dark-cornflower-blue"
-          />
-        </div>
-        <div className="flex flex-col items-center gap-6 bg-white p-16 px-6 shadow-xl">
-          <div className="bg-dark-cornflower-blue flex w-16 flex-none items-center justify-center rounded-full px-6 py-6 shadow-2xl">
-            <LinkedIn />
-          </div>
-          <h3 className="text-jet text-xl font-bold">
-            Code Of Business Conduct And Ethics
-          </h3>
-          <BorderButton
-            buttonText="View"
-            textColor="dark-cornflower-blue"
-            borderColor="dark-cornflower-blue"
-          />
-        </div>
-        <div className="flex flex-col items-center gap-6 bg-white p-16 px-6 shadow-xl">
-          <div className="bg-dark-cornflower-blue flex w-16 flex-none items-center justify-center rounded-full px-6 py-6 shadow-2xl">
-            <LinkedIn />
-          </div>
-          <h3 className="text-jet text-xl font-bold">
-            Code Of Business Conduct And Ethics
-          </h3>
-          <BorderButton
-            buttonText="View"
-            textColor="dark-cornflower-blue"
-            borderColor="dark-cornflower-blue"
-          />
-        </div>
-        <div className="flex flex-col items-center gap-6 self-center bg-white p-16 px-6 shadow-xl">
-          <div className="bg-dark-cornflower-blue flex w-16 flex-none items-center justify-center rounded-full px-6 py-6 shadow-2xl">
-            <LinkedIn />
-          </div>
-          <h3 className="text-jet text-xl font-bold">
-            Code Of Business Conduct And Ethics
-          </h3>
-          <BorderButton
-            buttonText="View"
-            textColor="dark-cornflower-blue"
-            borderColor="dark-cornflower-blue"
-          />
-        </div>
-        <div className="flex flex-col items-center gap-6 self-center bg-white p-16 px-6 shadow-xl">
-          <div className="bg-dark-cornflower-blue flex w-16 flex-none items-center justify-center rounded-full px-6 py-6 shadow-2xl">
-            <LinkedIn />
-          </div>
-          <h3 className="text-jet text-xl font-bold">
-            Code Of Business Conduct And Ethics
-          </h3>
-          <BorderButton
-            buttonText="View"
-            textColor="dark-cornflower-blue"
-            borderColor="dark-cornflower-blue"
-          />
-        </div>
-      </div>
-      <div className="mt-32 flex">
-        <div className="flex-none">
-          <PageNextPrevButton
-            imgName="investor-relations-program-next-prev.png"
-            pageName="Structures"
-            isLeft={true}
-          />
-        </div>
-        <div className="grow"></div>
-        <div className="flex-none">
-          <PageNextPrevButton
-            imgName="investor-relations-program-next-prev.png"
-            pageName="Investor Relations Program"
-            isLeft={false}
-          />
-        </div>
-      </div>
-    </section>
+    <>
+      <MainHeader
+        title={header.title}
+        breadcrumbs={breadcrumbs}
+        bgUrl={header.coverImage.url}
+        bgUrlSmall={header.smallCoverImage.url}
+        tabs={tabs}
+      />
+      <ManualCorporateGovernance content={content} />
+    </>
   );
 };
 
