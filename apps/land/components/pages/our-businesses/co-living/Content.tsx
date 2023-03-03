67df5b067a7430bb3e65198f1cd9d3fb68a384e8
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2023-03-03 09:10:02 +0800

    fix: fix implementation for connecting cms to ui

--- apps/land/components/pages/our-businesses/co-living/Content.tsx
+++ apps/land/components/pages/our-businesses/co-living/Content.tsx
@@ -1,68 +1,36 @@
 "use client";
-import React, { useState } from "react";
-import SelectLocation from "@/components/select/SelectLocation";
-import Projects from "@/components/list/Projects";
-import { LocationCategory, Project } from "shared-types";
-import useGetProjectsByLocation from "@/components/list/hooks/useGetProjectsByLocation";
+import React from "react";
+import TitleText from "./TitleText";
+import ProjectsContent from "./Projects";
+import Breadcrumbs from "@/components/header/Breadcrumbs";
+import MainHeader from "@/components/header/MainHeader";
 
-const Content = ({
-  projects,
-  locations,
-}: {
-  projects: Project[];
-  locations: LocationCategory[];
-}) => {
-  const [currentLocation, setCurrentLocation] = useState("");
-  const { data, isFetching } = useGetProjectsByLocation({
-    location: currentLocation,
-    projectType: "Co-living",
-  });
-  const locationString =
-    locations.length > 0
-      ? locations.map((location) => {
-          return location.title;
-        })
-      : [];
+const Content = ({ content, projects, locations }: any) => {
+  const header = content?.content.find(
+    (item: any) => item.blockType === "header"
+  );
+  const breadcrumbsItems = header?.breadcrumbs.map(
+    (tab: any, index: number) => {
+      return {
+        title: tab.link.label,
+        ...(index + 1 < header?.breadcrumbs?.length && { link: tab.link.url }),
+      };
+    }
+  );
+  const breadcrumbs = <Breadcrumbs items={breadcrumbsItems} />;
   return (
-    <section className="mx-9 mt-16 xl:mx-16 2xl:mx-44">
-      <div>
-        <div>
-          <h2 className="text-jet text-center text-4xl font-bold">
-            Erat nam at lectus urna duis
-          </h2>
-          <p className="text-dim-gray mt-8">
-            Adipiscing enim eu turpis egestas pretium aenean pharetra.
-            Consectetur a erat nam at lectus urna duis. Quam quisque id diam vel
-            quam elementum pulvinar etiam non. Enim ut tellus elementum sagittis
-            vitae et leo. Lacus sed viverra tellus in hac habitasse platea. In
-            ante metus dictum at tempor commodo. Cursus risus at ultrices mi
-            tempus.
-          </p>
-        </div>
-        <div className="mt-16 mb-24">
-          <div className="flex flex-col items-center gap-7 md:flex-row">
-            <div className="flex-1">
-              {!isFetching ? (
-                <h3 className="text-jet text-2xl">
-                  {data ? data.length : projects.length} co-living found
-                </h3>
-              ) : (
-                <h3 className="text-jet text-2xl">Loading...</h3>
-              )}
-            </div>
-            <div className="flex flex-none items-center gap-9">
-              <h3 className="text-jet text-xl">Filter by</h3>
-              <SelectLocation
-                locations={locationString}
-                setCurrentLocation={setCurrentLocation}
-                currentLocation={currentLocation}
-              />
-            </div>
-          </div>
-          <Projects projects={data ? data : projects} isLoading={isFetching} />
-        </div>
-      </div>
-    </section>
+    <>
+      <MainHeader
+        title={header.title}
+        breadcrumbs={breadcrumbs}
+        bgUrl={header.coverImage.url}
+        bgUrlSmall={header.smallCoverImage.url}
+      />
+      <section className="mx-9 mt-16 xl:mx-16 2xl:mx-44">
+        <TitleText content={content} />
+        <ProjectsContent projects={projects} locations={locations} />
+      </section>
+    </>
   );
 };
 
