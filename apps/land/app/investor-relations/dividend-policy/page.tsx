3064b43bb2c02f2cdadfed13f29ded8e9a8fb9ff
Marlou Lopez <dev.marlou.lopez@gmail.com>
2023-03-07 04:23:10 +0800

    remove unnecessary comment

--- apps/land/app/investor-relations/dividend-policy/page.tsx
+++ apps/land/app/investor-relations/dividend-policy/page.tsx
@@ -1,7 +1,6 @@
 import Content from "@/components/pages/investor-relations/dividend-policy/Content";
 import { getPageContent } from "../../page";
 import { metaBuilder } from "@/helpers/metaBuilder";
-//import { getPageContent } from "../../page";
 
 const DIVIDEND_POLICY_PAGE_ID = "640640c7f1b2d3552c7c5e02";
 export async function generateMetadata() {
