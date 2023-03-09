72600029516cda6b67e91cdb35f26f3945732a4c
Jomar Montuya <61284357+jomarmontuya@users.noreply.github.com>
2023-03-10 02:12:21 +0800

    fixes reports ands bullets design

--- apps/land/components/header/MainHeader.tsx
+++ apps/land/components/header/MainHeader.tsx
@@ -56,7 +56,7 @@ const MainHeader = ({
               src={otherUrl ? otherUrl : bgUrlUpdated}
               width={3844}
               height={1312}
-              alt="Picture of the author"
+              alt="bg"
               priority
             />
           </div>
@@ -64,18 +64,18 @@ const MainHeader = ({
             <Image
               src={bgUrlSmallUpdated}
               width={1036}
-              height={806}
-              alt="Picture of the author"
+              height={2400}
+              alt="bg"
               priority
             />
           </div>
         </section>
       ) : (
         <section className="z-0">
-          <div className="absolute w-full">
+          <div className="absolute hidden w-full xl:block">
             <div
               className={`flex flex-col items-center justify-center md:mt-0 ${
-                tabs ? "mt-6 py-28" : "mt-16  py-32 "
+                tabs ? "mt-6 py-28" : "mt-16 py-32 "
               }  md:py-24 lg:py-44 2xl:py-60`}
             >
               <h1
@@ -93,23 +93,40 @@ const MainHeader = ({
               {tabs}
             </div>
           </div>
-          <div className="z-0 hidden md:block">
+          <div className="z-0 hidden xl:block">
             <Image
               src={otherUrl ? otherUrl : bgUrlUpdated}
               width={3844}
               height={1312}
-              alt="Picture of the author"
+              alt="bg"
               priority
             />
           </div>
-          <div className="z-0 block md:hidden">
-            <Image
-              src={bgUrlSmallUpdated}
-              width={1036}
-              height={806}
-              alt="Picture of the author"
-              priority
-            />
+          <div
+            className="block w-full bg-cover bg-center bg-no-repeat xl:hidden"
+            style={{
+              backgroundImage: `url(${otherUrl ? otherUrl : bgUrlUpdated})`,
+            }}
+          >
+            <div
+              className={`flex flex-col items-center justify-center md:mt-0 ${
+                tabs ? " py-28" : "mt-16  py-32 "
+              }  md:py-24 lg:py-44 2xl:py-60`}
+            >
+              <h1
+                className={`mb-2 ${
+                  tabs ? "mt-4 text-2xl" : "-mt-4 text-3xl"
+                }  font-extrabold text-white md:mt-0 lg:mb-4 lg:text-6xl`}
+              >
+                {title}
+              </h1>
+              {breadcrumbs && (
+                <h5 className="text-center text-xs text-white lg:text-sm">
+                  {breadcrumbs}
+                </h5>
+              )}
+              {tabs}
+            </div>
           </div>
         </section>
       )}
