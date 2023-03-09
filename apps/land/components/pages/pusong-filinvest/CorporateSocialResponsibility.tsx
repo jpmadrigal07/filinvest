f2e2ad24613c4dbaa687bf3e7f6fba21e2fd8dfb
Vounts <jheeddi2@gmail.com>
2023-03-09 23:15:20 +0800

    Fix compile error 3

--- apps/land/components/pages/pusong-filinvest/CorporateSocialResponsibility.tsx
+++ apps/land/components/pages/pusong-filinvest/CorporateSocialResponsibility.tsx
@@ -14,18 +14,18 @@ const CorporateSocialResponsibility = ({ content }: any) => {
       <div className="mx-6 flex flex-col items-center justify-center gap-12 md:flex-row lg:mx-9 xl:mx-16 2xl:mx-44">
         <div className="flex-1">
           <Image
-            src={`${data.image.url}`}
+            src={`${data?.image?.url}`}
             width={1682}
             height={1288}
             alt="Picture of the author"
           />
         </div>
         <div className="flex-1">
-          <h3 className="text-dark-cornflower-blue font-bold">{data.title}</h3>
-          <h2 className="mt-4 text-4xl font-bold">{data.subTitle}</h2>
-          {data.descriptionParagraphs.map((item: any, index: number) => (
+          <h3 className="text-dark-cornflower-blue font-bold">{data?.title}</h3>
+          <h2 className="mt-4 text-4xl font-bold">{data?.subTitle}</h2>
+          {data?.descriptionParagraphs.map((item: any, index: number) => (
             <p className="text-dim-gray mt-2" key={index}>
-              {item.paragraph}
+              {item?.paragraph}
             </p>
           ))}
         </div>
