70dc3f0b1f1557b26b7f2e76d5efac7677c39a89
Marlou Lopez <dev.marlou.lopez@gmail.com>
2023-03-13 11:24:55 +0800

    remove related articles section

--- apps/land/components/pages/property-search/Content.tsx
+++ apps/land/components/pages/property-search/Content.tsx
@@ -3,15 +3,8 @@ import PropertySearch from "@/components/search/PropertySearch";
 import React, { useEffect, useState } from "react";
 import TileProjects from "@/components/list/TileProjects";
 import { Project } from "shared-types";
-import FeaturedArticles from "@/components/list/FeaturedArticles";
-import useGetNews from "../../../hooks/useGetNews";
-import { T_SearchQuery } from "@/types/global";
 
 const Content = () => {
-  const [searchParams] = useState<T_SearchQuery | null>(null);
-  const { data, refetch } = useGetNews({
-    searchParams,
-  });
   const [projects, setProjects] = useState<Project[]>([]);
   const [isPropertyLoading, setIsPropertyLoading] = useState<boolean>(false);
   useEffect(() => {
@@ -26,9 +19,8 @@ const Content = () => {
         showSearch={false}
         onLoading={setIsPropertyLoading}
         //setSearchParams={setSearchParams}
-        refetch={refetch}
       />
-      <div className="mx-9 mt-16 xl:mx-16 2xl:mx-44">
+      <div className="mx-9 mt-16 mb-24 xl:mx-16 2xl:mx-44">
         {!isPropertyLoading ? (
           <>
             <div className="text-xl">
@@ -40,20 +32,6 @@ const Content = () => {
         ) : (
           <div className="text-xl">Searching...</div>
         )}
-      </div>
-      <div className="bg-ghost-white mt-24 flex flex-col items-center py-24">
-        <section className="w-full">
-          <div className="mx-6 lg:mx-9 xl:mx-16 2xl:mx-44">
-            <h3 className="text-jet flex-none text-4xl font-bold">
-              Related Articles
-            </h3>
-            <FeaturedArticles
-              className="mt-16"
-              sliderOnMobile
-              articles={data ? data : []}
-            />
-          </div>
-        </section>
       </div>
     </section>
   );
